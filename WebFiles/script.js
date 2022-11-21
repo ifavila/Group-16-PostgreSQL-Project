d57ce1283637e99acc04731c3e4650614f80b4c8
ifavila <ifavila8@gmail.com>
2022-11-20 21:18:00 -0700

    finished shell for fetch from frontend

--- WebFiles/script.js
+++ WebFiles/script.js
@@ -1,9 +1,13 @@
 const displayButton = document.getElementById("displaybutton")
 const countryDisplay = document.getElementById("countrydisplay")
 
+
 function display(){
     countryDisplay.textContent = "Some new text!"
+    fetch("http://localhost:3000/countries")
+        .then((response) => response.json())
+        .then()
 }
 
 displayButton.addEventListener("click", display)
-fetch("http://localhost:3000/countries")
+
