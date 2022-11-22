7c852594cfd92a9ac16571d731ccdb94384478b2
ifavila <ifavila8@gmail.com>
2022-11-21 23:43:22 -0700

    front end and api are good

--- WebFiles/script.js
+++ WebFiles/script.js
@@ -3,10 +3,16 @@ const countryDisplay = document.getElementById("countrydisplay")
 
 
 function display(){
-    countryDisplay.textContent = "Some new text!"
+    //countryDisplay.textContent = "Some new text!"
+    countryDisplay.textContent = '';
+    
     fetch("http://localhost:3000/countries")
         .then((response) => response.json())
-        .then()
+        .then((data) => data.forEach(country => {
+            const countryItem = document.createElement("div")
+            countryItem.textContent = country.cname
+            countryDisplay.appendChild(countryItem)
+        }))
 }
 
 displayButton.addEventListener("click", display)
